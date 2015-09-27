078652327f70e319693007da9812090a32223de5
Leonardo Nascimento <leocamello@gmail.com>
2015-09-27 01:56:17 -0300

    Find Point Mathematics Challenge

---
+++ Domains/Mathematics/Fundamentals/FindPoint/FindPoint.CSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int T = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            string[] points = Console.ReadLine().Split(' ');
+
+            int Px, Py, Qx, Qy;
+
+            Px = Convert.ToInt32(points[0]);
+            Py = Convert.ToInt32(points[1]);
+            Qx = Convert.ToInt32(points[2]);
+            Qy = Convert.ToInt32(points[3]);
+
+            int Sx, Sy;
+            Sx = Qx + (Qx - Px);
+            Sy = Qy + (Qy - Py);
+
+            Console.Write("{0} {1}\n", Sx, Sy);
+        }
+    }
+}

217ae02a008ba05db1f6f150c5334aa7f087dd3b
Leonardo Nascimento <leocamello@gmail.com>
2015-09-27 03:16:29 -0300

    Sherlock and Moving Tiles Mathematics Fundamentals Challenge

---
+++ Domains/Mathematics/Fundamentals/SherlockAndMovingTiles/SherlockAndMovingTiles.CSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string[] LS1S2 = Console.ReadLine().Split(' ');
+
+        int L = Convert.ToInt32(LS1S2[0]);
+        int S1 = Convert.ToInt32(LS1S2[1]);
+        int S2 = Convert.ToInt32(LS1S2[2]);
+
+        double RS = Math.Abs(S1 - S2);
+
+        double D = Math.Sqrt((L * L) + (L * L));
+
+        int Q = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < Q; i++)
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            double d = Math.Sqrt(q + q);
+
+            double t = (D - d) / RS;
+
+            Console.WriteLine("{0:R}", t);
+        }
+    }
+}

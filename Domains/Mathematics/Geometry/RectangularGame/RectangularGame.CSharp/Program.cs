2910148c96c7aec4f6fcffec881636fd5852f337
Leonardo Nascimento <leocamello@gmail.com>
2015-09-28 20:17:31 -0300

    Rectangular Game Mathematics Geometry Challenge

---
+++ Domains/Mathematics/Geometry/RectangularGame/RectangularGame.CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int a, b;
+
+        a = b = int.MaxValue;
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] ab = Console.ReadLine().Split(' ');
+
+            a = Math.Min(a, Convert.ToInt32(ab[0]));
+            b = Math.Min(b, Convert.ToInt32(ab[1]));
+        }
+
+        Console.WriteLine(((double)a) * ((double)b));
+    }
+}

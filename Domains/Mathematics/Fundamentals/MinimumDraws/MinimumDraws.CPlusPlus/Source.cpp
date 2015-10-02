e2f5a6e5660ae0f36e69300691f8f73e58e92858
Leonardo Nascimento <leocamello@gmail.com>
2015-10-02 15:37:17 -0300

    Minimum Draws Mathematics Fundamentals Challenge

---
+++ Domains/Mathematics/Fundamentals/MinimumDraws/MinimumDraws.CPlusPlus/Source.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	int T;
+	cin >> T;
+
+	while (T--) {
+		int N;
+		cin >> N;
+
+		cout << N + 1 << endl;
+	}
+
+	return 0;
+}

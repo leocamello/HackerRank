7f775b4714f0e1b4535e508f46556d3c446b3589
Leonardo Nascimento <leocamello@gmail.com>
2015-10-10 20:48:27 -0300

    Hello World! Ruby Tutorial Introduction Challenge

--- Domains/Mathematics/Geometry/Stars/Stars.CSharp/Program.cs
+++ Domains/Mathematics/Geometry/Stars/Stars.CSharp/Program.cs
@@ -87,9 +87,9 @@ class Solution
     {
         int N = Convert.ToInt32(Console.ReadLine());
 
-        int[] x = new int[MAX_N];
-        int[] y = new int[MAX_N];
-        int[] v = new int[MAX_N];
+        int[] x = new int[N];
+        int[] y = new int[N];
+        int[] v = new int[N];
 
         for (int i = 0; i < N; i++)
         {
---
+++ Domains/Ruby/Introduction/README.md
@@ -0,0 +1,5 @@
+# Ruby Tutorial - Introduction
+
+| Title | Solution | Difficulty |
+| ----- | -------- | ---------- |
+| [Hello World!](https://www.hackerrank.com/challenges/ruby-hello-world) | [Ruby](./Hello%20World!/app.rb) | Easy |

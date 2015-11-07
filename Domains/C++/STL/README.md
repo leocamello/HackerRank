f7abf698491af3bf127df311e44b0f0a90d0ab6d
Leonardo Nascimento <leocamello@gmail.com>
2015-11-07 18:40:06 -0200

    Vector-Erase C++ Tutorial STL Challenge

--- Domains/C++/STL/README.md
+++ Domains/C++/STL/README.md
@@ -2,4 +2,5 @@
 
 | Title | Solution | Difficulty |
 | ----- | -------- | ---------- |
+| [Vector-Erase](https://www.hackerrank.com/challenges/vector-erase) | [C++](./Vector-Erase/main.cpp) | Easy |
 | [Vector-Sort](https://www.hackerrank.com/challenges/vector-sort) | [C++](./Vector-Sort/main.cpp) | Easy |
---
+++ Domains/C++/STL/Vector-Erase/main.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin >> N;
+
+    int vi;
+    vector<int> v;
+    while(N--) {
+        cin >> vi;
+        v.push_back(vi);
+    }
+
+    int x;
+    cin >> x;
+    v.erase(v.begin() + x - 1);
+    
+    int a, b;
+    cin >> a >> b;
+    v.erase(v.begin() + a - 1, v.begin() + b - 1);
+
+    cout << v.size() << endl;
+
+    int lastElement = v.size() - 1;
+    for (int i = 0; i < lastElement; i++) {
+        cout << v[i] << " ";
+    }
+
+    if (lastElement >= 0) {
+        cout << v[lastElement];
+    }
+
+    return 0;
+}

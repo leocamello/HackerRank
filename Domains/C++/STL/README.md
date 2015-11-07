b1e7bff68546690e89737ab4c62102daf721bd92
Leonardo Nascimento <leocamello@gmail.com>
2015-11-07 19:01:03 -0200

    Sets-STL C++ Tutorial STL Challenge

--- Domains/C++/STL/README.md
+++ Domains/C++/STL/README.md
@@ -2,6 +2,7 @@
 
 | Title | Solution | Difficulty |
 | ----- | -------- | ---------- |
+| [Sets-STL](https://www.hackerrank.com/challenges/cpp-sets) | [C++](./Sets-STL/main.cpp) | Easy |
 | [Lower Bound-STL](https://www.hackerrank.com/challenges/cpp-lower-bound) | [C++](./Lower%20Bound-STL/main.cpp) | Easy |
 | [Vector-Erase](https://www.hackerrank.com/challenges/vector-erase) | [C++](./Vector-Erase/main.cpp) | Easy |
 | [Vector-Sort](https://www.hackerrank.com/challenges/vector-sort) | [C++](./Vector-Sort/main.cpp) | Easy |
---
+++ Domains/C++/STL/Sets-STL/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int Q, y, x;
+    cin >> Q;
+
+    set<int> s;
+
+    while (Q--) {
+        cin >> y >> x;
+
+        switch (y) {
+            case 1:
+            s.insert(x);
+            break;
+            case 2:
+            s.erase(x);
+            break;
+            case 3:
+            if (s.find(x) != s.end()) {
+                cout << "Yes" << endl;
+            }
+            else {
+                cout << "No" << endl;
+            }
+        }
+    }
+
+    return 0;
+}

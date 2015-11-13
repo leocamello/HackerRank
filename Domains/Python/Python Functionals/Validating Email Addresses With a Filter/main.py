ed3e5b874e69734003522d83efb6dc0812933f05
Leonardo Nascimento <leocamello@gmail.com>
2015-11-12 23:36:49 -0200

    Validating Email Addresses With a Filter Python Tutorial Python Functionals Challenge

--- Domains/Python/Python Functionals/Validating Email Addresses With a Filter/main.py
+++ Domains/Python/Python Functionals/Validating Email Addresses With a Filter/main.py
@@ -1,40 +1,4 @@
-def isExtensionValid(extension):
-    if len(extension) == 0:
-        return False
-    if len(extension) > 3:
-        return False
-    return True
-
-def isWebsiteValid(website):
-    websiteValid = set('abcdefghijklmnopqrstuvwxyz0123456789')
-    if len(website) == 0:
-        return False
-    if len(set(website).difference(websiteValid)) > 0:
-        return False
-    return True
-
-def isUsernameValid(username):
-    usernameValid = set('_-abcdefghijklmnopqrstuvwxyz0123456789')
-    if len(username) == 0:
-        return False
-    if len(set(username).difference(usernameValid)) > 0:
-        return False
-    return True
-
-def isEmailValid(email):
-    res = email.split('@')
-    if len(res) != 2:
-        return False
-    if isUsernameValid(res[0]) != True:
-        return False
-    res = res[1].split('.')
-    if len(res) != 2:
-        return False
-    if isWebsiteValid(res[0]) != True:
-        return False
-    if isExtensionValid(res[1]) != True:
-        return False
-    return True
+import re
 
 N = int(input())
 
@@ -42,6 +6,6 @@ emails = list()
 for i in range(0, N):
     emails.append(input())
 
-emails.sort()
+regex = r'^[\w\d-]+@[A-Za-z0-9]+\.\w?\w?\w$'
 
-print (list(filter(lambda email : isEmailValid(email), emails)))
+print (sorted(list(filter(lambda email : re.search(regex, email), emails))))

be95dd3b0d495533915d2960babc5dc619029b7b
Leonardo Nascimento <leocamello@gmail.com>
2015-11-15 14:42:48 -0200

    HTML Parser - Part 1 Python Tutorial Regex and Parsing Challenge

---
+++ Domains/Python/Regex and Parsing/HTML Parser - Part 1/main.py
@@ -0,0 +1,22 @@
+from html.parser import HTMLParser
+
+def print_attrs(attrs):
+    for attr in attrs:
+        print ("->", attr[0], ">", attr[1])
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print ("Start :", tag)
+        print_attrs(attrs)
+    def handle_endtag(self, tag):
+        print ("End   :", tag)
+    def handle_startendtag(self, tag, attrs):
+        print ("Empty :", tag)
+        print_attrs(attrs)
+
+html = str()
+for i in range(int(input())):
+    html += input()
+
+parser = MyHTMLParser()
+parser.feed(html)

2c1fa101e5384bc96b9b6628ed6500076c83eef2
Leonardo Nascimento <leocamello@gmail.com>
2015-11-19 22:21:52 -0200

    Introduction Ruby Tutorial Strings Challenge

---
+++ Domains/Ruby/Strings/Introduction/main.rb
@@ -0,0 +1,13 @@
+def single_quote
+    return 'Hello World and others!'
+end
+
+def double_quote
+    return "Hello World and others!"
+end
+
+def here_doc
+    return <<-HERE
+        Hello World and others!
+    HERE
+end
--- Domains/Ruby/Strings/README.md
+++ Domains/Ruby/Strings/README.md
@@ -2,3 +2,4 @@
 
 | Title | Solution | Difficulty |
 | ----- | -------- | ---------- |
+| [Introduction](https://www.hackerrank.com/challenges/ruby-strings-introduction) | [Ruby](./Introduction/main.rb) | Easy |

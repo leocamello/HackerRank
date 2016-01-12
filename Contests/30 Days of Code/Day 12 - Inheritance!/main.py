bc440b2fee8fa03cf2b5b4a1629a7a8e64d86924
Leonardo Nascimento <leocamello@gmail.com>
2016-01-12 18:17:45 -0200

    Day 12 30 Days of Code Contest

---
+++ Contests/30 Days of Code/Day 12 - Inheritance!/main.py
@@ -0,0 +1,16 @@
+class Grade(Student):
+    def __init__(self, firstName, lastName, phone, score):
+        super().__init__(firstName, lastName, phone)
+        self.score = score
+    def calculate(self):
+        if self.score < 40:
+            return 'D'
+        elif self.score < 60:
+            return 'B'
+        elif self.score < 75:
+            return 'A'
+        elif self.score < 90:
+            return 'E'
+        else:
+            return 'O'
+   

707dd1927832a62c3d00c3071ec1ae0f38ae2ec9
Leonardo Nascimento <leocamello@gmail.com>
2016-01-13 19:51:25 -0200

    Day 13 30 Days Of Code Contest

---
+++ Contests/30 Days of Code/Day 13 - Abstract Classes!/main.py
@@ -0,0 +1,9 @@
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+    def display(self):
+        print(("Title: {0}").format(self.title))
+        print(("Author: {0}").format(self.author))
+        print(("Price: {0}").format(self.price))

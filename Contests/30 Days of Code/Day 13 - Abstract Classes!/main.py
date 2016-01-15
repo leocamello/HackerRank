71c9db10cce7c37875d66eae6e42fb5cda00f81c
Leonardo Nascimento <leocamello@gmail.com>
2016-01-14 22:58:20 -0200

    Day 13 30 Days of Code Contest update

--- Contests/30 Days of Code/Day 13 - Abstract Classes!/main.py
+++ Contests/30 Days of Code/Day 13 - Abstract Classes!/main.py
@@ -1,4 +1,10 @@
-#Write MyBook class
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    @abstractmethod
+    def display(): pass
 class MyBook(Book):
     def __init__(self, title, author, price):
         super().__init__(title, author)

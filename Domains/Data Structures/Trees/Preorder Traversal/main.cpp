52d359834353c75dfd5da5f9df461738800c578a
Leonardo Nascimento <leocamello@gmail.com>
2016-02-22 21:02:52 -0300

    Tree: Preorder Traversal Data Structures Trees Challenge

---
+++ Domains/Data Structures/Trees/Preorder Traversal/main.cpp
@@ -0,0 +1,23 @@
+/* you only have to complete the function given below.
+Node is defined as
+
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void Preorder(node *root) {
+    if (root != NULL) {
+        cout << root->data << " ";
+        Preorder(root->left);
+        Preorder(root->right);
+    }
+}
---
+++ Domains/Data Structures/Trees/README.md
@@ -0,0 +1,5 @@
+# Data Structures - Trees
+
+| Title | Solution | Difficulty |
+| ----- | -------- | ---------- |
+| [Tree: Preorder Traversal](https://www.hackerrank.com/challenges/tree-preorder-traversal) | [C++](./Preorder%20Traversal/main.cpp) | Easy |

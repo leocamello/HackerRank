db138326aefee5a1f81344bb27586f696cbc9b77
Leonardo Nascimento <leocamello@gmail.com>
2016-03-01 22:59:41 -0300

    Tree: Preorder Traversal (Iterative) Data Structures Trees Challenge

--- Domains/Data Structures/Trees/Preorder Traversal/main.cpp
+++ Domains/Data Structures/Trees/Preorder Traversal/main.cpp
@@ -1,23 +1,42 @@
 /* you only have to complete the function given below.
-Node is defined as
+   Node is defined as
 
-struct node
-{
+   struct node
+   {
     int data;
     node* left;
     node* right;
-};
+   };
 
-*/
+ */
 
 #include <iostream>
 
 using namespace std;
 
+// Recursive approach
 void Preorder(node *root) {
     if (root != NULL) {
         cout << root->data << " ";
         Preorder(root->left);
         Preorder(root->right);
+    }
+}
+
+#include <stack>
+
+// Iterative approach
+void Preorder(node *root) {
+    stack<node*> s;
+    s.push(root);
+    node* current;
+    while (!s.empty()) {
+        current = s.top();
+        s.pop();
+        if (current != NULL) {
+            cout << current->data << " ";
+            s.push(current->right);
+            s.push(current->left);
+        }
     }
 }

117c19b0f7d666f01d63b530839b84d2fa7e1959
Leonardo Nascimento <leocamello@gmail.com>
2016-03-01 23:49:34 -0300

    Tree: Inorder Traversal (Iterative) Data Structures Trees Challenge

--- Domains/Data Structures/Trees/Inorder Traversal/main.cpp
+++ Domains/Data Structures/Trees/Inorder Traversal/main.cpp
@@ -14,10 +14,31 @@ struct node
 
 using namespace std;
 
+// Recursive approach
 void Inorder(node *root) {
     if (root != NULL) {
         Inorder(root->left);
         cout << root->data << " ";
         Inorder(root->right);
     }
+}
+
+#include <stack>
+
+// Iterative approach
+void Inorder(node *root) {
+    stack<node*> s;
+    node* current = root;
+    do {
+        while (current != NULL) {
+            s.push(current);
+            current = current->left;
+        }
+        if (current == NULL && !s.empty()) {
+            current = s.top();
+            s.pop();
+            cout << current->data << " ";
+            current = current->right;
+        }
+    } while (current != NULL || !s.empty());
 }

d999cf06b03a5b915a910eb216d885f1fe9dac55
Leonardo Nascimento <leocamello@gmail.com>
2016-03-01 23:22:20 -0300

    Tree: Postorder Traversal (Iterative) Data Structures Trees Challenge

--- Domains/Data Structures/Trees/Postorder Traversal/main.cpp
+++ Domains/Data Structures/Trees/Postorder Traversal/main.cpp
@@ -14,10 +14,36 @@ struct node
 
 using namespace std;
 
+// Recursive approach
 void Postorder(node *root) {
     if (root != NULL) {
         Postorder(root->left);
         Postorder(root->right);
         cout << root->data << " ";
+    }
+}
+
+#include <stack>
+
+// Iterative approach
+void Postorder(node *root) {
+    stack<node*> s1, s2;
+    s1.push(root);
+    node* current;
+    while (!s1.empty()) {
+        current = s1.top();
+        s1.pop();
+        if (current != NULL) {
+            s2.push(current);
+            s1.push(current->left);
+            s1.push(current->right);
+        }
+    }
+    while (!s2.empty()) {
+        current = s2.top();
+        s2.pop();
+        if (current != NULL) {
+            cout << current->data << " ";
+        }
     }
 }

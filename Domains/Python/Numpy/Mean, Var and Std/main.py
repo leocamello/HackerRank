6af84c436e4eadfaaee835cc25517f9ad3cc865f
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-24 16:36:29 +0530

    Update main.py
    standard deviation has to be rounded to pass the test cases

--- Domains/Python/Numpy/Mean, Var and Std/main.py
+++ Domains/Python/Numpy/Mean, Var and Std/main.py
@@ -1,9 +1,8 @@
 import numpy
+numpy.set_printoptions(sign=' ')
+n, m = map(int, input().split())
+a = numpy.array([input().split() for _ in range(n)], float)
+print(numpy.mean(a, axis = 1)) 
+print (numpy.var(a, axis = 0))
+print(numpy.around(numpy.std(a), 12))
 
-N, M = map(int, input().split())
-
-A = numpy.array([list(map(int, input().split())) for n in range(N)])
-
-print (numpy.mean(A, axis = 1))
-print (numpy.var(A, axis = 0))
-print (numpy.std(A))

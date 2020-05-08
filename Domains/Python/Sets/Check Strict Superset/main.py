68b985eefc6bfcb1c92be184aaac35ea51b767d9
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-05-08 19:33:39 +0530

    Update main.py

--- Domains/Python/Sets/Check Strict Superset/main.py
+++ Domains/Python/Sets/Check Strict Superset/main.py
@@ -1,9 +1,3 @@
-A = set(map(int, raw_input().split()))
-
-res = True
-for i in range(0, int(raw_input())):
-    B = set(map(int, raw_input().split()))
-    if len(B.intersection(A)) != len(B) or len(B.difference(A)) != 0:
-        res = False
-
-print res
+#using generator
+a = set(input().split())
+print(all(a > set(input().split()) for _ in range(int(input()))))
